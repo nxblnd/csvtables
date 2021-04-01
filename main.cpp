aedbb8a2166c276b73e7e68422a39409e044bfe1
nxblnd <52958704+nxblnd@users.noreply.github.com>
2021-04-01 20:24:00 +0300

    Getting arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cout << "No filename provided" << std::endl;
+        return 1;
+    }
+    std::ifstream input(argv[1]);
+
     return 0;
 }
