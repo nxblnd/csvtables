0ecb34bf1e9753f8620bd8de6e55ade6958dbdc5
nxblnd <52958704+nxblnd@users.noreply.github.com>
2021-04-01 20:44:08 +0300

    Added get_header() function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+
+std::map<std::string, int> get_header(std::ifstream &input) {
+    std::string header;
+    std::getline(input, header);
+
+    std::stringstream ss(header);
+    int index = 0;
+    std::string tmp;
+    std::map<std::string, int> result;
+    while (std::getline(ss, tmp, ',')) {
+        result.insert(std::make_pair(tmp, index++));
+    }
+    return result;
+}
 
 int main(int argc, char **argv) {
     if (argc == 1) {
