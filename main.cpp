a850d0207811c7f4e8e16bec525c40f44db6947e
nxblnd <52958704+nxblnd@users.noreply.github.com>
2021-04-05 23:13:25 +0300

    Error catching in main()

--- main.cpp
+++ main.cpp
@@ -11,9 +11,14 @@ int main(int argc, char **argv) {
     }
 
     std::ifstream input(argv[1]);
-    Table t(input);
-    t.apply_formulas();
-    std::cout << t << std::endl;
+    try {
+        Table t(input);
+        t.apply_formulas();
+        std::cout << t << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << "Error occurred: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
